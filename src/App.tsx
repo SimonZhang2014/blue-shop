0cb5fb9306cb04f295e934466355480479d0401f
David Chen <chen7david@me.com>
2024-07-30 06:58:55 -0400

    chore: updated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { Router } from './pages/router/Router'
-
 function App() {
-  return <Router />
+  return (
+    <div className="flex bg-blue-500 h-[100dvh]">
+      <div className="w-60 fixed md:relative inset-y-0 bg-blue-700 md:-translate-x-full block">
+        1
+      </div>
+      <div className="flex-1 bg-blue-200">2</div>
+    </div>
+  )
 }
 
 export default App
